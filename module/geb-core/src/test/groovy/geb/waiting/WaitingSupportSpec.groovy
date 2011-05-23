3c46c64125cb50d099c0ba492523d0fd61fb5885
Luke Daley <ld@ldaley.com>
2011-05-24 08:17:06 +1000

    Moved spec into appropriate package

--- module/geb-core/src/test/groovy/geb/waiting/WaitingSupportSpec.groovy
+++ module/geb-core/src/test/groovy/geb/waiting/WaitingSupportSpec.groovy
@@ -12,10 +12,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package geb
+package geb.waiting
 
+import geb.*
 import geb.test.util.*
-import geb.waiting.WaitTimeoutException
 import spock.lang.*
 
 class WaitingSupportSpec extends GebSpecWithServer {
