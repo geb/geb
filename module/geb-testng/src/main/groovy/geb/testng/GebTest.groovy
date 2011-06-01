6083c2a2798d4dddbf56485057c799ae9a8b8264
Alexander Zolotov <zolotov@farpost.com>
2011-06-01 18:52:46 +1100

    Add licence text

--- module/geb-testng/src/main/groovy/geb/testng/GebTest.groovy
+++ module/geb-testng/src/main/groovy/geb/testng/GebTest.groovy
@@ -1,10 +1,26 @@
+/* Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *			http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package geb.testng
 
 import geb.Browser
 import org.openqa.selenium.WebDriver
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterMethod
 
 class GebTest {
+
 	private _browser
 
 	Browser getBrowser() {
@@ -19,7 +35,7 @@ class GebTest {
 	}
 
 	def methodMissing(String name, args) {
-		getBrowser()."$name"(*args)
+		getBrowser()."$name"(* args)
 	}
 
 	def propertyMissing(String name) {
