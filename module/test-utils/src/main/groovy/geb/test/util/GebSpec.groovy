ea8a92fc2256abc8b38d1fdaceb4b16e928c3bac
Luke Daley <ld@ldaley.com>
2011-06-02 15:30:45 +1000

    Copied geb-spock module into the test-utils module to break the circular dependency (of sorts) that IDEA can't handle.

--- module/test-utils/src/main/groovy/geb/test/util/GebSpec.groovy
+++ module/test-utils/src/main/groovy/geb/test/util/GebSpec.groovy
@@ -1,10 +1,11 @@
-/* Copyright 2009 the original author or authors.
+/* 
+ * Copyright 2011 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *			http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,25 +13,102 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package geb.test.util
 
-import geb.Browser
-import geb.spock.GebReportingSpec
+import geb.*
+import geb.report.*
+import spock.lang.*
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.htmlunit.HtmlUnitDriver
 
-class GebSpec extends GebReportingSpec {
+import org.junit.Rule
+import org.junit.rules.TestName
 
-	File getReportDir() {
-		new File("build/geb-output")
+class GebSpec extends Specification {
+
+	String gebConfEnv = null
+	String gebConfScript = null
+	
+	@Shared Browser _browser
+
+	// Ridiculous name to avoid name clashes
+	@Rule _gebReportingSpecTestName = new TestName()
+	def _gebReportingPerTestCounter = 0
+	@Shared _gebReportingSpecTestCounter = 0
+	@Shared _getReportingSpecReporter = null
+	
+	Configuration createConf() {
+		def conf = new ConfigurationLoader(gebConfEnv).getConf(gebConfScript)
+		conf
 	}
 	
 	Browser createBrowser() {
-		def browser = super.createBrowser()
+		def browser = new Browser(createConf())
 		if (browser.driver instanceof HtmlUnitDriver) {
 			browser.driver.javascriptEnabled = true
 		}
 		browser
 	}
 
+	Browser getBrowser() {
+		if (_browser == null) {
+			_browser = createBrowser()
+		}
+		_browser
+	}
+
+	void resetBrowser() {
+		if (_browser?.config.autoClearCookies) {
+			_browser.clearCookiesQuietly()
+		}
+		_browser = null
+	}
+
+	void report(String label) {
+		// We have to do this lazily here so the subclass gets a chance to run _some_ code to setup the reporter if need be.
+		// If we used setupSpec() that would run before the subclasses setupSpec() and limit the users options.
+		if (_gebReportingSpecTestCounter++ == 0) {
+			_getReportingSpecReporter = createReporter()
+		}
+		
+		def name = "${_gebReportingSpecTestCounter}-${++_gebReportingPerTestCounter}-${_gebReportingSpecTestName.methodName}"
+		if (label) {
+			name += "-$label"
+		}
+		
+		_getReportingSpecReporter?.writeReport(name, getBrowser())
+	}
+
+	Reporter createReporter() {
+		new ScreenshotAndPageSourceReporter(getReportDir(), this.class, true)
+	}
+
+	File getReportDir() {
+		new File("build/geb-output")
+	}
+	
+	def methodMissing(String name, args) {
+		getBrowser()."$name"(*args)
+	}
+
+	def propertyMissing(String name) {
+		getBrowser()."$name"
+	}
+	
+	def propertyMissing(String name, value) {
+		getBrowser()."$name" = value
+	}
+
+	private isSpecStepwise() {
+		this.class.getAnnotation(Stepwise) != null
+	}
+	
+	def cleanup() {
+		if (!isSpecStepwise()) resetBrowser()
+		report("end")
+	}
+	
+	def cleanupSpec() {
+		if (isSpecStepwise()) resetBrowser()
+	}
 }
