db21c0b7d4aa6c1d1dc748ca9b8117e4eed430cd
Christopher Kolstad <Christopher.Kolstad@iteraconsulting.no>
2011-12-08 18:38:47 +0000

    Starting point

---
+++ module/geb-core/src/main/groovy/geb/navigator/SelectionContext.groovy
@@ -0,0 +1,11 @@
+package geb.navigator
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: chriswk
+ * Date: 12/8/11
+ * Time: 6:37 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class SelectionContext {
+}
