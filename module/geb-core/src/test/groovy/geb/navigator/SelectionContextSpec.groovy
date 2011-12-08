de370dd2cd8f92ec253929851c8dbba3f9afb1cb
Christopher Kolstad <Christopher.Kolstad@iteraconsulting.no>
2011-12-08 20:54:27 +0000

    Refactor tests for DRY

--- module/geb-core/src/test/groovy/geb/navigator/SelectionContextSpec.groovy
+++ module/geb-core/src/test/groovy/geb/navigator/SelectionContextSpec.groovy
@@ -1,11 +1,8 @@
 package geb.navigator
 
-import geb.test.util.GebSpec
-import spock.lang.Shared
-import org.openqa.selenium.WebDriver
-import geb.textmatching.TextMatchingSupport
-import geb.test.util.GebSpecWithServer
 import geb.Page
+import geb.test.util.GebSpecWithServer
+import spock.lang.Unroll
 
 class SelectionContextSpec extends GebSpecWithServer {
 
@@ -24,37 +21,24 @@ class SelectionContextSpec extends GebSpecWithServer {
             }
         }
     }
-    
-    def "Verify we can get the CSS selector for the firstDiv element"() {
-        when:
-            to SelectionPage
-        then:
-            firstDiv.selectionContext.selector == "div#test-1"
-            
-    }
-    
-    def "Verify we can get the CSS selector for the secondDiv element"() {
-        when:
-          to SelectionPage
-        then:
-          secondDiv.selectionContext.selector == "div#test-2"
-    }
 
-    def "Verify non-matching selector has selection context selector"() {
-        when:
-            to SelectionPage
-        then:
-            nonMatchingElement.selectionContext.selector == "div#non-matching"
-    }
-    
-    def "Verify that we get child's selector string from subordinate"() {
-        when:
-            to SelectionPage
-        then:
-            firstDivParagraph.selectionContext.selector == "p#testParagraph"
+    @Unroll("element #element has selector #selector")
+    def "selector matching"() {
+        given:
+        to SelectionPage
+
+        expect:
+        page."$element".selectionContext.selector == selector
+
+        where:
+        element              | selector
+        "firstDiv"           | "div#test-1"
+        "secondDiv"          | "div#test-2"
+        "nonMatchingElement" | "div#non-matching"
+        "firstDivParagraph"  | "p#testParagraph"
 
     }
-    
+
 }
 class SelectionPage extends Page {
     static content = {
