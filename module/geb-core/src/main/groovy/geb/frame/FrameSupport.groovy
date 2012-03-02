88677bafc55d86dbd97d3891be8c7c9e2cf3ac72
Eric Sword <eric.r.sword@saic.com>
2012-03-02 12:23:02 -0500

    GEB-173 - support for switching to frame and page in one call

--- module/geb-core/src/main/groovy/geb/frame/FrameSupport.groovy
+++ module/geb-core/src/main/groovy/geb/frame/FrameSupport.groovy
@@ -5,6 +5,8 @@ import org.openqa.selenium.NoSuchFrameException
 import geb.Browser
 import geb.navigator.Navigator
 import geb.content.SimplePageContent
+import geb.Page
+import geb.waiting.WaitTimeoutException
 
 class FrameSupport {
 
@@ -24,11 +26,15 @@ class FrameSupport {
 	}
 
 	private withFrameForContent(content, Closure block) {
+		withFrame(getElement(content), block)
+	}
+
+	private WebElement getElement(content) {
 		WebElement element = content.firstElement()
 		if (element == null) {
 			throw new NoSuchFrameException("No elements for given content: $content")
 		}
-		withFrame(element, block)
+		return element
 	}
 
 	def withFrame(Navigator frame, Closure block) {
@@ -37,5 +43,39 @@ class FrameSupport {
 
 	def withFrame(SimplePageContent frame, Closure block) {
 		withFrameForContent(frame, block)
+	}
+
+	/**
+	 * Changes the browser's focus to the given frame and calls {@link geb.Browser#page} on the given page.
+	 * @param content A {@link geb.navigator.Navigator}, {@link geb.content.SimplePageContent}, int or String.
+	 * 		If a Navigator or SimplePageContent, the first WebElement is used.
+	 * @param newPage a Class object that extends {@link geb.Page}, and Page instance, or an array of potential
+	 * 		page classes.
+	 */
+	void frame(content, newPage) {
+		switchToFrame(content)
+		browser.page(newPage)
+	}
+
+	/**
+	 * Changes focus to the frame or iframe matched by the given content, waiting for the content nav
+	 * to return true first is necessary.
+	 * @param content
+	 * @return
+	 */
+	protected switchToFrame(content) {
+		def element
+		if ((content instanceof Navigator) || (content instanceof SimplePageContent)) {
+			//if waitFor fails, throw a more descriptive exception
+			try {
+				browser.page.waitFor{content}
+			} catch (WaitTimeoutException e) {
+				throw new NoSuchFrameException("No elements for given content: $content")
+			}
+			element = getElement(content)
+		}
+		else
+			element = content
+		browser.driver.switchTo().frame(element)
 	}
 }
