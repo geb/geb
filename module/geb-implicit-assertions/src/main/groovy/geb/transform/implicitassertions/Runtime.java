fd455dcd13815eff854c9baf902d4d3b40c44029
Luke Daley <ld@ldaley.com>
2012-03-16 11:16:11 +0000

    Make the implicit transform runtime helper Java.

---
+++ module/geb-implicit-assertions/src/main/groovy/geb/transform/implicitassertions/Runtime.java
@@ -0,0 +1,28 @@
+package geb.transform.implicitassertions;
+
+import groovy.lang.MetaClass;
+import groovy.lang.MetaMethod;
+import org.codehaus.groovy.runtime.InvokerHelper;
+
+public abstract class Runtime {
+	public static boolean isVoidMethod(Object target, String method, Object... args) {
+		
+		Class[] argTypes = new Class[args.length];
+		int i = 0;
+		for (Object arg : args) {
+			argTypes[i++] = arg == null ? null : arg.getClass();
+		} 
+
+		MetaClass metaClass = InvokerHelper.getMetaClass(target);
+
+		MetaMethod metaMethod = metaClass.pickMethod(method, argTypes);
+		if (metaMethod == null) {
+			return false;
+		}
+
+		Class returnType = metaMethod.getReturnType();
+		return returnType == void.class || returnType == Void.class;
+	}
+
+
+}
