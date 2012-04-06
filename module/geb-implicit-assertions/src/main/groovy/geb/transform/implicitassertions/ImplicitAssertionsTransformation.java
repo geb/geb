d5bf1a1670d2b84b6bde06528aadf0d0ca3612f7
Luke Daley <ld@ldaley.com>
2012-04-06 18:52:54 +0100

    Use correct visit method

--- module/geb-implicit-assertions/src/main/groovy/geb/transform/implicitassertions/ImplicitAssertionsTransformation.java
+++ module/geb-implicit-assertions/src/main/groovy/geb/transform/implicitassertions/ImplicitAssertionsTransformation.java
@@ -1,18 +1,17 @@
 package geb.transform.implicitassertions;
 
-import geb.transform.implicitassertions.ImplicitAssertionsTransformationVisitor;
-import org.codehaus.groovy.ast.ClassNode;
-import org.codehaus.groovy.transform.GroovyASTTransformation;
-import org.codehaus.groovy.control.CompilePhase;
-import org.codehaus.groovy.transform.ASTTransformation;
 import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.ast.ClassNode;
+import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
+import org.codehaus.groovy.transform.ASTTransformation;
+import org.codehaus.groovy.transform.GroovyASTTransformation;
 
 @GroovyASTTransformation(phase = CompilePhase.CANONICALIZATION)
 public class ImplicitAssertionsTransformation implements ASTTransformation {
     public void visit(ASTNode[] astNodes, SourceUnit sourceUnit) {
 		for (ClassNode classNode : sourceUnit.getAST().getClasses()) {
-			classNode.visit(new ImplicitAssertionsTransformationVisitor(sourceUnit));
+			classNode.visitContents(new ImplicitAssertionsTransformationVisitor(sourceUnit));
 		}
     }
 }
