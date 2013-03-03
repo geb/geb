5789a73b899ef79b3f8d7f81a5b7dcec6cc293db
Luke Daley <ld@ldaley.com>
2013-03-03 20:53:39 +0000

    Cut this test down to just test the interact block wiring and navigator unpacking.

--- module/geb-core/src/test/groovy/geb/interaction/InteractionsSupportSpec.groovy
+++ module/geb-core/src/test/groovy/geb/interaction/InteractionsSupportSpec.groovy
@@ -1,111 +1,36 @@
 package geb.interaction
 
-import geb.Page
 import geb.test.CrossBrowser
 import geb.test.GebSpecWithServer
-import org.openqa.selenium.Keys
 
 @CrossBrowser
 class InteractionsSupportSpec extends GebSpecWithServer {
 
-	def setupSpec() {
-		responseHtml { request ->
+	def "navigators are unpacked in interact block"() {
+		given:
+		html {
 			body {
 				input(id: 'first-input', value: '')
 				input(id: 'second-input', value: '')
-				form(id: 'form', name: 'form') {
-					select(id: 'select-element', name: 'select_element') {
-						(1..3).each {
-							option(value: "${it}", "Element ${it}")
-						}
-						option(value: '4', selected: 'selected', "Element 4")
-					}
-				}
 			}
 		}
-	}
 
-	def "Move between elements using interactions"() {
-
-		given:
-		to TestHtmlPage
 		$('#first-input').click()
 
 		when:
 		interact {
-			moveToElement($('#second-input'))
+			moveToElement $('#first-input')
 			click()
-			sendKeys('geb')
-			moveToElement($('#first-input'))
+			sendKeys 'GEB'
+			moveToElement $('#second-input')
 			click()
-			keyDown(Keys.SHIFT)
-			sendKeys('GEB')
-			keyUp(Keys.SHIFT)
+			sendKeys 'geb'
 		}
 
 		then:
-		$('#second-input').value() == 'geb'
 		$('#first-input').value() == 'GEB'
-
+		$('#second-input').value() == 'geb'
 	}
 
-	def "Move between content defined elements using interactions"() {
-
-		given:
-		to TestHtmlPage
-		firstInput.click()
-
-		when:
-
-		interact {
-			moveToElement(secondInput)
-			click()
-			sendKeys('geb')
-			moveToElement(firstInput)
-			click()
-			keyDown(Keys.SHIFT)
-			sendKeys('GEB')
-			keyUp(Keys.SHIFT)
-		}
-
-		then:
-		secondInput.value() == 'geb'
-		firstInput.value() == 'GEB'
-
-	}
-
-	def "Focus various elements by moving to and clicking them"() {
-
-		given:
-		to TestHtmlPage
-
-		expect:
-		$('form').select_element == '4'
-
-		when:
-		interact {
-			moveToElement($('#select-element option', value: '4'))
-			click()
-			moveToElement($('#select-element option', value: '2'))
-			click()
-		}
-
-		then:
-		$('form').select_element == '2'
-
-	}
-
-}
-
-class TestHtmlPage extends Page {
-
-	static url = '/interactor'
-
-	static content = {
-
-		firstInput { $('#first-input') }
-		secondInput { $('#second-input') }
-
-	}
 
 }
