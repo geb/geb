cc9f054ac3dacd24dfb80eb758a0823db3d2a878
Marcin Erdmann <erdi84@gmail.com>
2014-05-04 00:53:15 +0100

    GEB-276 Add safari to the set of browsers tested against using saucelabs
    * update to the latest webdriver
    * test against latest chrome and firefox
    * test against ie10 and ie11
    * ensure that webdriver version used at saucelabs is the same as locally

--- internal/test-support/src/main/resources/SpockConfig.groovy
+++ internal/test-support/src/main/resources/SpockConfig.groovy
@@ -1,6 +1,12 @@
 import geb.test.CrossBrowser
+import geb.interaction.InteractionsSupportSpec
 
-def usingSauce = System.getProperty("geb.sauce.browser")
-if (usingSauce) {
-	runner { include CrossBrowser }
+def sauceBrowser = System.getProperty("geb.sauce.browser")
+if (sauceBrowser) {
+	runner {
+		include CrossBrowser
+		if (sauceBrowser.startsWith('safari')) {
+			exclude InteractionsSupportSpec
+		}
+	}
 }
