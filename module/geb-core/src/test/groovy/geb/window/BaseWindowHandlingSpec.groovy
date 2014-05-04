cc023a0faedb20977c1c690fa48cdedd87019b35
Marcin Erdmann <erdi84@gmail.com>
2014-05-04 17:13:42 +0100

    Always make sure that window can be accessed by name after opening for tests in BaseWindowHandlingSpec

--- module/geb-core/src/test/groovy/geb/window/BaseWindowHandlingSpec.groovy
+++ module/geb-core/src/test/groovy/geb/window/BaseWindowHandlingSpec.groovy
@@ -63,5 +63,12 @@ abstract class BaseWindowHandlingSpec extends GebSpecWithServer {
 
 	protected void openWindow(int index) {
 		$("a", index - 1).click()
+
+		def originalWindowHandle = currentWindow
+		//ensure that we can switch to the new window by name, for some drivers (IE, Safari) it's not instant
+		waitFor(40) {
+			switchToWindow(windowName(index))
+			switchToWindow(originalWindowHandle)
+		}
 	}
 }
