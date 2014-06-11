56862460863a79f1eeb0c359709c9eec086a7ec7
Chris Prior <chris.prior@underwriteme.co.uk>
2014-06-11 08:34:52 +0100

    GEB-278 Verify pages at checker when passed as an option to open a new window via withWindow, withNewWindow

--- module/geb-core/src/main/groovy/geb/Browser.groovy
+++ module/geb-core/src/main/groovy/geb/Browser.groovy
@@ -585,6 +585,8 @@ class Browser {
 				}
 
 				if (specification.call()) {
+					verifyAtIfPresent(options.page)
+
 					try {
 						block.call()
 					} finally {
@@ -618,9 +620,8 @@ class Browser {
 
 		switchToWindow(window)
 		try {
-			if (options.page) {
-				page(options.page)
-			}
+			verifyAtIfPresent(options.page)
+
 			block.call()
 		} finally {
 			if (options.close) {
@@ -651,9 +652,8 @@ class Browser {
 		def newWindow = executeNewWindowOpening(windowOpeningBlock, options.wait)
 		try {
 			switchToWindow(newWindow)
-			if (options.page) {
-				page(options.page)
-			}
+			verifyAtIfPresent(options.page)
+
 			block.call()
 		} finally {
 			if (!options.containsKey('close') || options.close) {
@@ -821,6 +821,17 @@ class Browser {
 			new URL(uri.toString() + joiner + queryString).toString()
 		} else {
 			uri.toString()
+		}
+	}
+
+	private void verifyAtIfPresent(Class<? extends Page> targetPage) {
+		if (targetPage) {
+			try {
+				at(targetPage)
+			}
+			catch (UndefinedAtCheckerException e) {
+				page(targetPage)
+			}
 		}
 	}
 
