8cb9f4bb75be10c319e85bde631e1da62ebecd17
Marcin Erdmann <erdi84@gmail.com>
2015-04-16 22:28:06 +0100

    Make examples in the section on composing navigators executable.
    Part of geb/issues#350

---
+++ doc/new-manual/src/test/groovy/navigator/CompositionSpec.groovy
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package navigator
+
+import geb.Page
+import geb.test.GebSpecWithCallbackServer
+import org.openqa.selenium.By
+
+class CompositionSpec extends GebSpecWithCallbackServer {
+
+    def setup() {
+        responseHtml """
+            <html>
+                // tag::html[]
+                    <p class="a">1</p>
+                    <p class="b">2</p>
+                    <p class="c">3</p>
+                // end::html[]
+            </html>
+        """
+        go()
+    }
+
+    def "dollar"() {
+        expect:
+        // tag::dollar[]
+        $($("p.a"), $("p.b"))*.text() == ["1", "2"]
+        // end::dollar[]
+    }
+
+    def "add"() {
+        expect:
+        // tag::add[]
+        $("p.a").add("p.b").add(By.className("c"))*.text() == ["1", "2", "3"]
+        // end::add[]
+    }
+
+    def "content"() {
+        when:
+        page CompositionSpecPage
+
+        then:
+        // tag::content[]
+        $(pElement("a"), pElement("b"))*.text() == ["1", "2"]
+        // end::content[]
+    }
+}
+
+class CompositionSpecPage extends Page {
+    // tag::content_definition[]
+    static content = {
+        pElement { pClass -> $('p', class: pClass) }
+    }
+    // end::content_definition[]
+}
