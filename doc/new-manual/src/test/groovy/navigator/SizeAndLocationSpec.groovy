6a0b79c06f3e5001e608e3c65b5c55f30df84e1d
Marcin Erdmann <erdi84@gmail.com>
2015-04-18 21:53:32 +0100

    Make examples in the section on navigator elements' size and location executable.
    Part of geb/issues#350

---
+++ doc/new-manual/src/test/groovy/navigator/SizeAndLocationSpec.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package navigator
+
+import geb.test.GebSpecWithCallbackServer
+
+class SizeAndLocationSpec extends GebSpecWithCallbackServer {
+
+    def setup() {
+        html """
+            <html>
+                // tag::html[]
+                <div style="height: 20px; width: 40px; position: absolute; left: 20px; top: 10px"></div>
+                <div style="height: 40px; width: 100px; position: absolute; left: 30px; top: 150px"></div>
+                // end::html[]
+            </html>
+        """
+    }
+
+    def "single element"() {
+        expect:
+        // tag::single_element[]
+        $("div").height == 20
+        $("div").width == 40
+        $("div").x == 20
+        $("div").y == 10
+        // end::single_element[]
+    }
+
+    def "muliple elements"() {
+        expect:
+        // tag::multiple_elements[]
+        $("div")*.height == [20, 40]
+        $("div")*.width == [40, 100]
+        $("div")*.x == [20, 30]
+        $("div")*.y == [10, 150]
+        // end::multiple_elements[]
+    }
+}
