b0a9a4b0d441981513b1358e046ee8ac49715ef6
Marcin Erdmann <erdi84@gmail.com>
2015-06-07 23:57:50 +0100

    Make the examples in the introduction section for modules executable.
    Part of geb/issues#350

---
+++ doc/new-manual/src/test/groovy/modules/IntroductionSpec.groovy
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package modules
+
+import fixture.Browser
+import fixture.DriveMethodSupportingSpecWithServer
+import geb.Module
+import geb.Page
+
+class IntroductionSpec extends DriveMethodSupportingSpecWithServer {
+
+    def setup() {
+        server.html {
+            form(id: "personal-data") {
+                input(id: "button", type: "button", "click me!")
+            }
+            script(type: "text/javascript", """
+                var button = document.getElementById("button");
+                button.onclick = function () {
+                    button.className = "clicked";
+                };
+            """)
+        }
+    }
+
+    def "using modules"() {
+        expect:
+        // tag::using_modules[]
+        Browser.drive {
+            // end::using_modules[]
+            driver.javascriptEnabled = true
+            // tag::using_modules[]
+            to ModulePage
+            form.button.click()
+            // end::using_modules[]
+            assert form.button.hasClass("clicked")
+            // tag::using_modules[]
+        }
+        // end::using_modules[]
+    }
+
+    def "using parameterized modules"() {
+        expect:
+        // tag::using_parameterized_modules[]
+        Browser.drive {
+            // end::using_parameterized_modules[]
+            driver.javascriptEnabled = true
+            // tag::using_parameterized_modules[]
+            to ParameterizedModulePage
+            form("personal-data").button.click()
+            // end::using_parameterized_modules[]
+            assert form("personal-data").button.hasClass("clicked")
+            // tag::using_parameterized_modules[]
+        }
+        // end::using_parameterized_modules[]
+    }
+
+    def "using nested modules"() {
+        expect:
+        // tag::using_nested_modules[]
+        Browser.drive {
+            // end::using_nested_modules[]
+            driver.javascriptEnabled = true
+            // tag::using_nested_modules[]
+            to OuterModulePage
+            outerModule.form.button.click()
+            // end::using_nested_modules[]
+            assert outerModule.form.button.hasClass("clicked")
+            // tag::using_nested_modules[]
+        }
+        // end::using_nested_modules[]
+    }
+}
+
+// tag::form_module[]
+class FormModule extends Module {
+    static content = {
+        button { $("input", type: "button") }
+    }
+}
+// end::form_module[]
+
+// tag::module_page[]
+class ModulePage extends Page {
+    static content = {
+        form { module FormModule }
+    }
+}
+// end::module_page[]
+
+// tag::parameterized_module[]
+class ParameterizedModule extends Module {
+    String formId
+    static content = {
+        button {
+            $("form", id: formId).find("input", type: "button")
+        }
+    }
+}
+// end::parameterized_module[]
+
+// tag::parameterized_module_page[]
+class ParameterizedModulePage extends Page {
+    static content = {
+        form { id -> module(new ParameterizedModule(formId: id)) }
+    }
+}
+// end::parameterized_module_page[]
+
+// tag::outer_module_and_page[]
+class OuterModule extends Module {
+    static content = {
+        form { module FormModule }
+    }
+}
+
+class OuterModulePage extends Page {
+    static content = {
+        outerModule { module OuterModule }
+    }
+}
+// end::outer_module_and_page[]
