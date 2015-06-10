d053629169300ea7896e37ad371b3856deba19ad
Marcin Erdmann <erdi84@gmail.com>
2015-06-10 14:35:58 +0100

    Make the examples in the section on configuring the driver executable.
    Part of geb/issues#350

---
+++ doc/new-manual/src/test/groovy/configuration/DriverConfigSpec.groovy
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package configuration
+
+import geb.Configuration
+import geb.ConfigurationLoader
+import geb.driver.CachingDriverFactory
+import geb.test.WebDriverServer
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.remote.RemoteWebDriver
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class DriverConfigSpec extends Specification {
+
+    @Rule
+    TemporaryFolder temporaryFolder
+
+    Configuration config
+
+    def setupSpec() {
+        CachingDriverFactory.clearCacheAndQuitDriver()
+    }
+
+    void configScript(String env = null, String script) {
+        def classLoader = new GroovyClassLoader(getClass().classLoader)
+        classLoader.addClasspath(temporaryFolder.root.absolutePath)
+        def configFile = temporaryFolder.newFile()
+        configFile << script
+        config = new ConfigurationLoader(env, null, classLoader).getConf(configFile.name)
+    }
+
+    def "configuring driver using closure"() {
+        when:
+        configScript """
+            // tag::configuring_driver[]
+            import org.openqa.selenium.firefox.FirefoxDriver
+
+            driver = { new FirefoxDriver() }
+            // end::configuring_driver[]
+        """
+
+        then:
+        config.driver instanceof FirefoxDriver
+
+        cleanup:
+        CachingDriverFactory.clearCacheAndQuitDriver()
+    }
+
+    def "configuring driver using class name"() {
+        when:
+        configScript """
+            // tag::configuring_driver_using_class_name[]
+            driver = "org.openqa.selenium.firefox.FirefoxDriver"
+            // end::configuring_driver_using_class_name[]
+        """
+
+        then:
+        config.driver instanceof FirefoxDriver
+
+        cleanup:
+        CachingDriverFactory.clearCacheAndQuitDriver()
+    }
+
+    def "configuring driver using driver name"() {
+        when:
+        configScript """
+            // tag::configuring_driver_using_driver_name[]
+            driver = "firefox"
+            // end::configuring_driver_using_driver_name[]
+        """
+
+        then:
+        config.driver instanceof FirefoxDriver
+
+        cleanup:
+        CachingDriverFactory.clearCacheAndQuitDriver()
+    }
+
+    @Unroll("driver should be #driverClass.simpleName when environment is #env")
+    def "environment sensitive driver config"() {
+        given:
+        def webDriverServer = new WebDriverServer()
+        webDriverServer.start()
+
+        when:
+        configScript(env, """
+            // tag::env_sensitive_driver_config[]
+            import org.openqa.selenium.htmlunit.HtmlUnitDriver
+
+            import org.openqa.selenium.remote.DesiredCapabilities
+            import org.openqa.selenium.remote.RemoteWebDriver
+
+            // default is to use htmlunit
+            driver = { new HtmlUnitDriver() }
+
+            environments {
+                // when system property 'geb.env' is set to 'remote' use a remote Firefox driver
+                remote {
+                    driver = {
+                        def remoteWebDriverServerUrl = new URL("http://example.com/webdriverserver")
+                        // end::env_sensitive_driver_config[]
+                        remoteWebDriverServerUrl = new URL("${webDriverServer.getBaseUrl()}")
+                        // tag::env_sensitive_driver_config[]
+                        new RemoteWebDriver(remoteWebDriverServerUrl, DesiredCapabilities.firefox())
+                    }
+                }
+            }
+
+            // end::env_sensitive_driver_config[]
+        """)
+
+        then:
+        config.driver in driverClass
+
+        cleanup:
+        CachingDriverFactory.clearCacheAndQuitDriver()
+        webDriverServer.stop()
+
+        where:
+        env      | driverClass
+        null     | HtmlUnitDriver
+        "remote" | RemoteWebDriver
+    }
+}
