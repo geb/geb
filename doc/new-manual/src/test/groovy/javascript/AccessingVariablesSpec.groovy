5cd1b40dc8c8624bfc497ec39b2d9dbb14b5231f
Marcin Erdmann <erdi84@gmail.com>
2015-06-18 13:02:02 +0100

    Make the examples in the section on accessing js variables executable.
    Part of geb/issues#350

---
+++ doc/new-manual/src/test/groovy/javascript/AccessingVariablesSpec.groovy
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package javascript
+
+import fixture.Browser
+import fixture.DriveMethodSupportingSpecWithServer
+import geb.Page
+
+class AccessingVariablesSpec extends DriveMethodSupportingSpecWithServer {
+
+    def htmlWithJsVariable() {
+        server.html """
+            // tag::html[]
+            <html>
+                <head>
+                    <script type="text/javascript">
+                        var aVariable = 1;
+                    </script>
+                </head>
+            </html>
+            // end::html[]
+        """
+    }
+
+    def "accessing a variable"() {
+        given:
+        htmlWithJsVariable()
+
+        expect:
+        // tag::accessing[]
+        Browser.drive {
+            go "/"
+            assert js.aVariable == 1
+        }
+        // end::accessing[]
+    }
+
+    def "mapping a variable to content"() {
+        given:
+        htmlWithJsVariable()
+
+        expect:
+        // tag::mapping[]
+        Browser.drive {
+            to JsVariablePage
+            assert aVar == 1
+        }
+        // end::mapping[]
+    }
+
+    def "accessing nested variables"() {
+        given:
+        server.html {
+            title "Book of Geb"
+        }
+
+        expect:
+        Browser.drive {
+            go()
+            // tag::nested[]
+            assert js."document.title" == "Book of Geb"
+            // end::nested[]
+        }
+    }
+}
+
+class JsVariablePage extends Page {
+    static content = {
+        aVar { js.aVariable }
+    }
+}
