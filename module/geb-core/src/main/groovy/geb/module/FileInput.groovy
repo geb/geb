8ace1bb82f84925ef76fdfd1058c398dd78a97bc
Marcin Erdmann <erdi84@gmail.com>
2015-06-30 09:11:06 +0100

    Add module class for file input.
    Part of geb/issues#303

---
+++ module/geb-core/src/main/groovy/geb/module/FileInput.groovy
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package geb.module
+
+import geb.error.InvalidModuleBaseException
+
+class FileInput extends FormElement {
+
+    @Override
+    protected void initialized() {
+        if (!empty) {
+            if (tag().toLowerCase() != "input") {
+                throw new InvalidModuleBaseException("Specified base element for ${FileInput.name} module was '${tag()}' but only input is allowed as the base element.")
+            }
+            def type = getAttribute("type")
+            if (type != "file") {
+                throw new InvalidModuleBaseException("Specified base element for ${FileInput.name} module was an input of type '$type' but only input of type text is allowed as the base element.")
+            }
+        }
+    }
+
+    File getFile() {
+        def path = value()
+        path ? new File(path) : null
+    }
+
+    void setFile(File file) {
+        value(file.absolutePath)
+    }
+}
