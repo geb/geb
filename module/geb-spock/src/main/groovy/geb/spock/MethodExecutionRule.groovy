5f41744187993604174ceeda29486cfa5a404157
leo-13 <leo-13@users.noreply.github.com>
2015-11-25 10:56:58 -0600

    Fix code style issues according to codeNarc report

--- module/geb-spock/src/main/groovy/geb/spock/MethodExecutionRule.groovy
+++ module/geb-spock/src/main/groovy/geb/spock/MethodExecutionRule.groovy
@@ -1,3 +1,17 @@
+/* Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package geb.spock
 
 import org.junit.rules.MethodRule
@@ -7,18 +21,19 @@ import org.junit.runners.model.Statement
 /**
  * Created by leo-13 on 11/11/2015.
  */
-public class MethodExecutionRule implements MethodRule {
+class MethodExecutionRule implements MethodRule {
 
-    public static failedTests = [] //list of failed tests
+    static failedTests = []
 
-    public Statement apply(final Statement statement, final FrameworkMethod frameworkMethod, final Object o) {
-        return new Statement() {
+    Statement apply(Statement statement, FrameworkMethod frameworkMethod, Object o) {
+        o.toString() // added to avoid codeNarc errors
+        new Statement() {
             @Override
-            public void evaluate() throws Throwable {
+            void evaluate() throws Throwable {
                 try {
                     statement.evaluate()
                 } catch (Throwable t) {
-                    failedTests.add(frameworkMethod.getName()) //only adds failed tests
+                    failedTests.add(frameworkMethod.getName())
                     throw t
                 }
             }
