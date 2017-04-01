af64ac15da590282e37281abf3de10aad4b323de
Antony Jones <aj@desirableobjects.co.uk>
2017-04-01 15:05:56 +0100

    fix minor spacing issues

--- doc/site/src/ratpack/public/js/base.js
+++ doc/site/src/ratpack/public/js/base.js
@@ -1,7 +1,18 @@
 $('.menu .manuals')
     .popup({
-        inline: true,
         hoverable: true,
+        exclusive: true,
+        position: 'bottom left',
+        delay: {
+            show: 300,
+            hide: 800
+        }
+    });
+
+$('.menu .apis')
+    .popup({
+        hoverable: true,
+        exclusive: true,
         position: 'bottom left',
         delay: {
             show: 300,
