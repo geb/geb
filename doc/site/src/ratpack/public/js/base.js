3dccc6901eb5f8865a1eeb82cb225f983afe01d0
Antony Jones <aj@desirableobjects.co.uk>
2017-04-01 12:19:21 +0100

    fix up manuals menu

---
+++ doc/site/src/ratpack/public/js/base.js
@@ -0,0 +1,10 @@
+$('.menu .manuals')
+    .popup({
+        inline: true,
+        hoverable: true,
+        position: 'bottom left',
+        delay: {
+            show: 300,
+            hide: 800
+        }
+    });
