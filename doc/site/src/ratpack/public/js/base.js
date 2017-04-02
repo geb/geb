0d802c29a61d156f025e0b171ad8abc409048863
Antony Jones <aj@desirableobjects.co.uk>
2017-04-02 11:46:06 +0100

    add mailing lists popup

--- doc/site/src/ratpack/public/js/base.js
+++ doc/site/src/ratpack/public/js/base.js
@@ -1,5 +1,6 @@
 $('.menu .manuals')
     .popup({
+        popup: '#manuals-popup',
         hoverable: true,
         exclusive: true,
         position: 'bottom left',
@@ -11,6 +12,7 @@ $('.menu .manuals')
 
 $('.menu .apis')
     .popup({
+        popup: '#apis-popup',
         hoverable: true,
         exclusive: true,
         position: 'bottom left',
@@ -19,3 +21,30 @@ $('.menu .apis')
             hide: 800
         }
     });
+
+$('.menu .mailing-lists')
+    .popup({
+        popup: '#mailing-lists-popup',
+        hoverable: true,
+        exclusive: true,
+        position: 'bottom left',
+        delay: {
+            show: 300,
+            hide: 800
+        }
+    });
+
+
+$.get('https://api.github.com/repos/geb/geb')
+    .done(function(body) {
+        var watchers = body.subscribers_count;
+        var stars = body.stargazers_count;
+        var forks = body.network_count;
+        $('[data-stars]').text(stars);
+        $('[data-watchers]').text(watchers);
+        $('[data-forks]').text(forks);
+    })
+    .catch(function(e) {
+        console.error('Github api request failed. No stats available.')
+        $('[data-github-stats] > .statistic:not(.static)').transition('fade')
+    })
