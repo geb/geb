6a96d8d5a8a27c8d4d1f84d153328c35ed4f99dc
Marcin Erdmann <erdi84@gmail.com>
2017-10-08 17:37:12 +0100

    Update to Groovy 2.5.0-beta-2

--- doc/manual-snippets/src/test/groovy/assertions/ImplicitAssertionsSpec.groovy
+++ doc/manual-snippets/src/test/groovy/assertions/ImplicitAssertionsSpec.groovy
@@ -35,7 +35,7 @@ Assertion failed:
 title == "Implicit Assertions!"
 |     |
 |     false
-Something else
+'Something else'
 // end::at_checker_message[]
 """
     }
@@ -48,7 +48,7 @@ Assertion failed:
 title == "Page Title"
 |     |
 |     false
-Something else
+'Something else'
 // end::waiting_message[]
 """
     }
