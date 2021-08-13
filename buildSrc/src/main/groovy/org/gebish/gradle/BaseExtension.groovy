98dded9b945c1e1b40fdbf58c79603c19dd17a1b
Marcin Erdmann <erdi84@gmail.com>
2021-08-13 15:51:24 +0100

    Start moving away from allprojects/subprojects blocks and towards precompiled script plugins

---
+++ buildSrc/src/main/groovy/org/gebish/gradle/BaseExtension.groovy
@@ -0,0 +1,18 @@
+package org.gebish.gradle
+
+import org.gradle.api.Project
+
+class BaseExtension {
+
+    private final Project project
+
+    BaseExtension(Project project) {
+        this.project = project
+    }
+
+    void onCi(Closure closure) {
+        if (project.hasProperty("ci")) {
+            project.configure(project, closure)
+        }
+    }
+}
