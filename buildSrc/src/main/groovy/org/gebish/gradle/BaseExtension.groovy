206f271e5c5765e9b66fbbc0dbce2a39373daa52
Marcin Erdmann <erdi84@gmail.com>
2021-08-28 12:02:53 +0100

    Use a method on the extension object added by the base plugin rather than a project extension property for setting parallelism of some test tasks

--- buildSrc/src/main/groovy/org/gebish/gradle/BaseExtension.groovy
+++ buildSrc/src/main/groovy/org/gebish/gradle/BaseExtension.groovy
@@ -2,6 +2,8 @@ package org.gebish.gradle
 
 import org.gradle.api.Project
 
+import static java.lang.Runtime.getRuntime
+
 class BaseExtension {
 
     private final Project project
@@ -11,12 +13,20 @@ class BaseExtension {
     }
 
     void onCi(Closure closure) {
-        if (project.hasProperty("ci")) {
+        if (isCi()) {
             project.configure(project, closure)
         }
     }
 
     boolean isSnapshot() {
         project.version.endsWith("-SNAPSHOT")
+    }
+
+    int getMaxWorkers() {
+        isCi() ? 2 : Math.min(runtime.availableProcessors().intdiv(2), 4)
+    }
+
+    private boolean isCi() {
+        project.hasProperty("ci")
     }
 }
